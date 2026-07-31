420aab5698bc3981879b338fcae43942d9d769b3
maximpje <wynantmaxim@gmail.com>
2026-07-31 23:23:31 +0200

    beta1

--- lua/sex.lua
+++ lua/sex.lua
@@ -1,13 +1,36 @@
 local M = {}
 
-function M.sex()
-    local buf = vim.api.nvim_ceate_buf(false, true)
+function girl(goon_dir)
+    local goonfile = goon_dir .. '1.txt'
+    local i = 0
+    local lua_goon_table = {}
 
-    local sexwindow = vim.api.nvim_open_win(buf, false, {relative='0', width=60, height=80})
+    for line in io.lines(goonfile) do
+        table.insert(lua_goon_table, line)
+        i = i+1
+    end
+
+    return lua_goon_table
 end
 
-function M.setup()
-    vim.api.nvim_create_user_command('SexOpen', M.sex(), {})
+function M.setup(goon_dir, width, height)
+
+    vim.api.nvim_create_user_command('SexOpen', function()
+        local buf = vim.api.nvim_create_buf(false, true)
+
+        vim.api.nvim_buf_set_lines(buf, 0, -1, true, girl(goon_dir))
+
+        local sexwindow = vim.api.nvim_open_win(buf, false, {
+            relative='win',
+            width=width,
+            height=height,
+            col=vim.api.nvim_win_get_width(0),
+            row=0,
+            border={"╔", "═" ,"╗", "║", "╝", "═", "╚", "║"},
+            anchor='NE',
+            style='minimal',
+        })
+    end, {})
 end
 
 return M
